f5bf600675fe319a80200892b3359acab5854b12
saambpatil@outlook.com <saambpatil@outlook.com>
2024-12-23 15:07:01 +0530

    ..

--- src/pages/Todo/Todo.jsx
+++ src/pages/Todo/Todo.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+/* eslint-disable no-unused-vars */
+import  { useEffect, useState } from "react";
 import axios from "axios";
 import AddTodo from "../../components/AddTodo/AddTodo";
 import TodoList from "../../components/TodoList/TodoList";
